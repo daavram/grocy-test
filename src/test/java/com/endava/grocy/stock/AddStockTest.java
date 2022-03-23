bceda2209fb60dd4468790f238d7312943541c98
Daniel Avram <daniel.avram@endava.com>
2022-03-23 16:45:10 +0200

    Jira-19 - improve shouldAddTwoStocksSameProduct test result validation

--- src/test/java/com/endava/grocy/stock/AddStockTest.java
+++ src/test/java/com/endava/grocy/stock/AddStockTest.java
@@ -6,6 +6,7 @@ import com.endava.grocy.model.Stock;
 import com.endava.grocy.model.TransactionType;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.is;
@@ -16,27 +17,20 @@ public class AddStockTest extends TestBaseClass {
     @Test
     public void shouldAddStock() {
         //GIVEN
-        grocyFixture.createEntity(EntityType.LOCATION)
-                .createEntity(EntityType.QUANTITY_UNIT)
-                .createEntity(EntityType.PRODUCTS);
+        grocyFixture.createEntity(EntityType.LOCATION).createEntity(EntityType.QUANTITY_UNIT).createEntity(EntityType.PRODUCTS);
         Integer productId = grocyFixture.getProduct().getId();
         Stock stock = dataProvider.getStock();
         //WHEN
         Response response = stockClient.addStock(productId, stock);
         //THEN
-        response.then().statusCode(HttpStatus.SC_OK)
-                .body("size()", is(1))
-                .body("id[0]", is(notNullValue()))
-                .body("amount[0]", is(stock.getAmount().toString()));
+        response.then().statusCode(HttpStatus.SC_OK).body("size()", is(1)).body("id[0]", is(notNullValue())).body("amount[0]", is(stock.getAmount().toString()));
     }
 
     //BUG JIRA
     @Test
     public void shouldFailAddStockGivenTransactionTypeNotPurchase() {
         //GIVEN
-        grocyFixture.createEntity(EntityType.LOCATION)
-                .createEntity(EntityType.QUANTITY_UNIT)
-                .createEntity(EntityType.PRODUCTS);
+        grocyFixture.createEntity(EntityType.LOCATION).createEntity(EntityType.QUANTITY_UNIT).createEntity(EntityType.PRODUCTS);
         Integer productId = grocyFixture.getProduct().getId();
         Stock stock = dataProvider.getStock();
         stock.setTransactionType(TransactionType.CONSUME);
@@ -45,19 +39,14 @@ public class AddStockTest extends TestBaseClass {
         Response response = stockClient.addStock(productId, stock);
 
         //THEN
-        response.then().statusCode(HttpStatus.SC_OK)
-                .body("size()", is(1))
-                .body("id[0]", is(notNullValue()))
-                .body("amount[0]", is(stock.getAmount().toString()));
+        response.then().statusCode(HttpStatus.SC_OK).body("size()", is(1)).body("id[0]", is(notNullValue())).body("amount[0]", is(stock.getAmount().toString()));
     }
 
     //BUG JIRA
     @Test
     public void shouldFailAddStockGivenNegativeAmount() {
         //GIVEN
-        grocyFixture.createEntity(EntityType.LOCATION)
-                .createEntity(EntityType.QUANTITY_UNIT)
-                .createEntity(EntityType.PRODUCTS);
+        grocyFixture.createEntity(EntityType.LOCATION).createEntity(EntityType.QUANTITY_UNIT).createEntity(EntityType.PRODUCTS);
         Integer productId = grocyFixture.getProduct().getId();
         Stock stock = dataProvider.getStock();
         stock.setAmount(-120.5);
@@ -66,19 +55,14 @@ public class AddStockTest extends TestBaseClass {
         Response response = stockClient.addStock(productId, stock);
 
         //THEN
-        response.then().statusCode(HttpStatus.SC_OK)
-                .body("size()", is(1))
-                .body("id[0]", is(notNullValue()))
-                .body("amount[0]", is(stock.getAmount().toString()));
+        response.then().statusCode(HttpStatus.SC_OK).body("size()", is(1)).body("id[0]", is(notNullValue())).body("amount[0]", is(stock.getAmount().toString()));
     }
 
     //BUG JIRA
     @Test
     public void shouldFailAddStockGivenNegativePrice() {
         //GIVEN
-        grocyFixture.createEntity(EntityType.LOCATION)
-                .createEntity(EntityType.QUANTITY_UNIT)
-                .createEntity(EntityType.PRODUCTS);
+        grocyFixture.createEntity(EntityType.LOCATION).createEntity(EntityType.QUANTITY_UNIT).createEntity(EntityType.PRODUCTS);
         Integer productId = grocyFixture.getProduct().getId();
         Stock stock = dataProvider.getStock();
         stock.setPrice(-120.5);
@@ -87,10 +71,7 @@ public class AddStockTest extends TestBaseClass {
         Response response = stockClient.addStock(productId, stock);
 
         //THEN
-        response.then().statusCode(HttpStatus.SC_OK)
-                .body("size()", is(1))
-                .body("id[0]", is(notNullValue()))
-                .body("amount[0]", is(stock.getAmount().toString()));
+        response.then().statusCode(HttpStatus.SC_OK).body("size()", is(1)).body("id[0]", is(notNullValue())).body("amount[0]", is(stock.getAmount().toString()));
     }
 
     @Test
@@ -102,16 +83,13 @@ public class AddStockTest extends TestBaseClass {
         Response response = stockClient.addStock(Integer.MAX_VALUE, stock);
 
         //THEN
-        response.then().statusCode(HttpStatus.SC_BAD_REQUEST)
-                .body("error_message", is("Product does not exist or is inactive"));
+        response.then().statusCode(HttpStatus.SC_BAD_REQUEST).body("error_message", is("Product does not exist or is inactive"));
     }
 
     @Test
     public void shouldFailedAddStockDeletedProduct() {
         //GIVEN
-        grocyFixture.createEntity(EntityType.LOCATION)
-                .createEntity(EntityType.QUANTITY_UNIT)
-                .createEntity(EntityType.PRODUCTS);
+        grocyFixture.createEntity(EntityType.LOCATION).createEntity(EntityType.QUANTITY_UNIT).createEntity(EntityType.PRODUCTS);
         Integer productId = grocyFixture.getProduct().getId();
         entityClient.deleteEntityById(EntityType.PRODUCTS, productId);
         Stock stock = dataProvider.getStock();
@@ -120,17 +98,14 @@ public class AddStockTest extends TestBaseClass {
         Response response = stockClient.addStock(productId, stock);
 
         //THEN
-        response.then().statusCode(HttpStatus.SC_BAD_REQUEST)
-                .body("error_message", is("Product does not exist or is inactive"));
+        response.then().statusCode(HttpStatus.SC_BAD_REQUEST).body("error_message", is("Product does not exist or is inactive"));
 
     }
 
     @Test
     public void shouldAddStockOtherLocation() {
         //GIVEN
-        grocyFixture.createEntity(EntityType.LOCATION)
-                .createEntity(EntityType.QUANTITY_UNIT)
-                .createEntity(EntityType.PRODUCTS);
+        grocyFixture.createEntity(EntityType.LOCATION).createEntity(EntityType.QUANTITY_UNIT).createEntity(EntityType.PRODUCTS);
         Integer productId = grocyFixture.getProduct().getId();
         Integer locationId = grocyFixture.getLocation().getId();
         Stock stock = dataProvider.getStock();
@@ -138,38 +113,27 @@ public class AddStockTest extends TestBaseClass {
         //WHEN
         Response response = stockClient.addStock(productId, stock);
         //THEN
-        response.then().statusCode(HttpStatus.SC_OK)
-                .body("size()", is(1))
-                .body("id[0]", is(notNullValue()))
-                .body("amount[0]", is(stock.getAmount().toString()));
+        response.then().statusCode(HttpStatus.SC_OK).body("size()", is(1)).body("id[0]", is(notNullValue())).body("amount[0]", is(stock.getAmount().toString()));
 
     }
 
+    @DisplayName("Add two stocks for the same product")
     @Test
     public void shouldAddTwoStocksSameProduct() {
+
         //GIVEN
-        grocyFixture.createEntity(EntityType.LOCATION)
-                .createEntity(EntityType.QUANTITY_UNIT)
-                .createEntity(EntityType.PRODUCTS);
+        grocyFixture.createEntity(EntityType.LOCATION).createEntity(EntityType.QUANTITY_UNIT).createEntity(EntityType.PRODUCTS);
         Integer productId = grocyFixture.getProduct().getId();
         Stock stockOne = dataProvider.getStock();
         Stock stockTwo = dataProvider.getStock();
 
         //WHEN
-        Response responseStockOne = stockClient.addStock(productId, stockOne);
-        Response responseStockTwo = stockClient.addStock(productId, stockTwo);
+        stockClient.addStock(productId, stockOne);
+        stockClient.addStock(productId, stockTwo);
+        String currentStockAmount = String.format("%.2f", stockOne.getAmount() + stockTwo.getAmount());
         Response responseStockCurrent = stockClient.getStockById(productId);
-        //THEN
-        responseStockOne.then().statusCode(HttpStatus.SC_OK)
-                .body("size()", is(1))
-                .body("id[0]", is(notNullValue()))
-                .body("amount[0]", is(stockOne.getAmount().toString()));
-        responseStockTwo.then().statusCode(HttpStatus.SC_OK)
-                .body("size()", is(1))
-                .body("id[0]", is(notNullValue()))
-                .body("amount[0]", is(stockTwo.getAmount().toString()));
-        responseStockCurrent.then().statusCode(HttpStatus.SC_OK);
 
+        //THEN
+        responseStockCurrent.then().statusCode(HttpStatus.SC_OK).body("stock_amount", is(currentStockAmount));
     }
-//
 }
